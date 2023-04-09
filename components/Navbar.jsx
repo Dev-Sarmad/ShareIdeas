4fd21b15d52474dba51fc1148cda9dc9ef12e0fe
Dev-Sarmad <sarmadh230@gmail.com>
2023-04-09 09:34:08 +0500

    userLogin and logoutlogic in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,38 @@
 import Link from "next/link";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Navbar() {
-  return <nav className="flex justify-between py-10">
-   <Link href="/">
-   <button className="bg-black text-white text-2xl font-medium px-4">Share ideas</button>
-   </Link>
-   <ul className="flex items-center">
-    {/* this will redirectes to the pages/auth/login */}
-    <Link href={"/auth/login"}>
-    <button className="text-white bg-green-600 rounded font-bold">Join Now</button>
-    </Link>
-   </ul>
-  </nav>;
+  const [user, loading] = useAuthState(auth);
+  console.log(user);
+  return (
+    <nav className="flex justify-between py-10">
+      <Link href="/">
+        <button className="bg-black text-white text-2xl font-medium px-4">
+          Share ideas
+        </button>
+      </Link>
+      <ul className="flex items-center">
+        {/* this will redirectes to the pages/auth/login */}
+        {!user && (
+          <Link href={"/auth/login"}>
+          <button className="text-white bg-green-600 rounded font-bold">
+            Join Now
+          </button>
+        </Link>
+        )}
+        {user && (
+          <div className="flex items-center gap-5">
+            <Link href="/post">
+            <button className="text-white bg-green-600 px-4 font-sans text-xl rounded">Post</button>
+            </Link>
+            <Link href={'/dashboard'}>
+            <img src={user.photoURL} alt="" className="w-12 rounded-full " />
+              {/* <h1 className="font-medium capitalize cursor-pointer">Dashboard</h1> */}
+            </Link>
+          </div>
+        )}
+      </ul>
+    </nav>
+  );
 }
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,18 +1,31 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 export default function Login() {
-    const route = useRouter()
-    const googleProvider = new GoogleAuthProvider();
-    const googleLogin = async () => {
-      try {
-        const result = await signInWithPopup(auth, googleProvider);
-        route.redirect("/")
-      } catch(error) {
-        console.log(error);
-      }
-    };
+  // provides the user(auth) is logged in or not
+  const [user, loading] = useAuthState(auth);
+  const route = useRouter();
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      route.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //   it runs everytime when the user is changed/logged in/logged out
+  useEffect(() => {
+    if (user) {
+      route.push("/");
+    } else {
+      console.log("login");
+    }
+  }, [user]);
+  
   return (
     <div className="rounded p-10 shadow-xl text-center mt-32 text-gray-700 items-center">
       <h2 className="font-medium text-3xl">Join Now</h2>
